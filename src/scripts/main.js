51cb1b36a273bd9a5a492e42a5ddb6096a4a059e
Christopher Humphries <engleek@Engleek-Work.local.cidre.rennes.supelec.fr>
2014-11-17 18:13:20 +0100

    First commit. Basic bespoke presentation. Some slides.

---
+++ src/scripts/main.js
@@ -0,0 +1,22 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  nebula = require('bespoke-theme-nebula'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  backdrop = require('bespoke-backdrop'),
+  scale = require('bespoke-scale'),
+  ga = require('bespoke-ga'),
+  hash = require('bespoke-hash');
+
+// Bespoke.js
+bespoke.from('article', [
+  nebula(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
+  backdrop(),
+  scale(),
+  hash(),
+  ga({ trackingId: 'UA-44850660-3' })
+]);
